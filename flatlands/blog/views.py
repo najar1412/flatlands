be1801ffc722cdfb6748bcfbd2c306f4022a3ab0
najar1412 <rory.jarrel@gmail.com>
2018-06-27 01:49:08 -0400

    initial commit

---
+++ flatlands/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse, get_object_or_404
+
+from .models import BlogPost
+
+
+def index(request):
+    blog_posts = BlogPost.objects.order_by('-pub_date')
+    context = {'blog_posts': blog_posts}
+
+
+    return render(request, 'blog/index.html', context)
+
+
+def blog_post(request, post_id):
+    post = get_object_or_404(BlogPost, pk=post_id)
+    
+
+    return render(request, 'blog/post.html', {'post': post})
