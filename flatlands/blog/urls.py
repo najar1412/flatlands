d55e4929f98a7ba4deae98cb50953ef2569c99f9
najar1412 <rory.jarrel@gmail.com>
2018-06-27 17:56:48 +0100

    fleshing out design

--- flatlands/blog/urls.py
+++ flatlands/blog/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_id>/', views.blog_post, name='blog_post'),
+    path('code/', views.code, name='code'),
+    path('about/', views.about, name='about'),
+    path('search/', views.search, name='search'),
 ]
--- flatlands/blog/views.py
+++ flatlands/blog/views.py
@@ -7,7 +7,6 @@ def index(request):
     blog_posts = BlogPost.objects.order_by('-pub_date')
     context = {'blog_posts': blog_posts}
 
-
     return render(request, 'blog/index.html', context)
 
 
@@ -15,3 +14,15 @@ def blog_post(request, post_id):
     post = get_object_or_404(BlogPost, pk=post_id)
 
     return render(request, 'blog/post.html', {'post': post})
+
+
+def code(request):
+    return render(request, 'blog/code.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+def search(request):
+    return render(request, 'blog/search.html')
